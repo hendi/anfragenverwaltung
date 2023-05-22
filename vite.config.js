25d745c5edb5422b86a0f37eb49235d864c1caee
Patrick Ecker <ryyppy@users.noreply.github.com>
2023-05-22 09:28:36 +0200

    Add VITE_PORT env variable for easy port manipulation when running npm start

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8000,
+    port: process.env.VITE_PORT || 8000,
   },
   plugins: [react()],
 });
