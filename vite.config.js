a9ccc24b6d53a1622522614fb220f8aadae8dfa9
Timothy <1695613+timothyis@users.noreply.github.com>
2023-06-14 18:02:51 +0200

    Add error tracking for message area

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   server: {
     port: process.env.VITE_PORT || 8000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    })
+  ],
 });
